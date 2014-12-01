ae8d30036ce05517af5c3ab7922c68bdf26ab860
Graham Heath <Graham.Heath@rga.com>
2014-12-01 16:17:08 -0600

    basic example 1 working using directive for stage and service for pixi

--- scripts/view1/view1.js
+++ scripts/view1/view1.js
@@ -9,6 +9,35 @@ angular.module('PixiPalace.view1', ['ngRoute','PixiPalace.Pixi'])
   });
 }])
 
-.controller('View1Ctrl', ['PixiPalacePixiService',function() {
+.controller('View1Ctrl', ['$scope','PixiPalacePixiService',function($scope, PixiPalacePixiService) {
 
+  $scope.addABunny = function () {
+    var pixiService = PixiPalacePixiService;
+
+    var texture = pixiService.pixi.Texture.fromImage("images/bunny.png");
+    var bunny = new pixiService.pixi.Sprite(texture);
+
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
+
+    bunny.position.x = Math.random() * pixiService.renderer.view.width;
+    bunny.position.y = Math.random() * pixiService.renderer.view.height;
+    
+    bunny.speed = Math.floor((Math.floor(Math.random() * 10) / 2));
+    
+    pixiService.stage.addChild(bunny);
+    
+    function animate() {
+      requestAnimFrame(animate);
+
+      // just for fun, lets rotate mr rabbit a little
+      bunny.rotation += bunny.speed;
+
+      // render the stage
+      pixiService.renderer.render(pixiService.stage);
+    }
+    requestAnimFrame(animate);
+    //------------
+  };
+  
 }]);
